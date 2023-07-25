a8a39d9f334a1e22f044cbad1b381cc0ab29ba9a
AjilAshok <aj4ashok@gmail.com>
2023-07-25 18:41:35 +0530

    update the pagination

--- lib/view/homepage.dart
+++ lib/view/homepage.dart
@@ -2,20 +2,43 @@ import 'package:flutter/material.dart';
 import 'package:paginationone/controller/auth_controller.dart';
 import 'package:paginationone/view/logine_view.dart';
 import 'package:provider/provider.dart';
+import 'package:number_paginator/number_paginator.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int currentpage = 1;
   @override
   Widget build(BuildContext context) {
     final authController = Provider.of<AuthController>(context);
     return Scaffold(
       appBar: AppBar(
+        elevation: 0,
         automaticallyImplyLeading: false,
         title: const Text('HomePage'),
       ),
-      body: const Center(
-        child: Text('You are loged in'),
+      body: Column(
+        children: [
+          const SizedBox(
+            height: 650,
+            child: Center(
+              child: Text('You are loged in'),
+            ),
+          ),
+          NumberPaginator(
+            numberPages: 20,
+            onPageChange: (index) {
+              setState(() {
+                currentpage = index;
+              });
+            },
+          )
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
